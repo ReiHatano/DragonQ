6397dedb7b11193446d5be3ddda636bc3ce9ec7d
ReiHatano <rei.hatano@gmail.com>
2015-12-03 23:45:07 +0900

    オルテガの攻撃パターン追加中１

--- js/test.js
+++ js/test.js
@@ -1,8 +1,9 @@
 
-
+//キャラクター誕生
 var ortega = {name:"オルテガ", hp:120, strength:12}
 var shidoh = {name:"シドー", hp:300, strength:18}
 
+//ステータスの表示
 var nameElement = document.getElementById("name");
 nameElement.innerHTML  = ortega.name
 
@@ -12,11 +13,25 @@ hpElement.innerHTML = ortega.hp
 var strengthElement = document.getElementById("strength");
 strengthElement.innerHTML = ortega.strength
 
-function attack(a){
-  shidoh.hp -= a*2
+//オルテガの攻撃パターン
+function attack1(){
+  var takeDamagePoint = (1+Math.floor(Math.random()*10)) + ortega.strength
+  shidoh.hp -= takeDamagePoint
+  document.write("<p>オルテガの攻撃！</p>"+shidoh.name+"に"+takeDamagePoint+"ポイントのダメージを与えた！");
 };
 
-attack(50);
+function attack2(){
+  var takeDamagePoint = ((1+Math.floor(Math.random()*10)) + ortega.strength)*2
+  shidoh.hp -= takeDamagePoint
+  document.write("<p>オルテガの攻撃！</p>会心の一撃！"+shidoh.name+"に"+takeDamagePoint+"ポイントのダメージを与えた！");
+};
+
+
+
+//実行クラス
+
+attack1();
+attack2();
 
 
 
