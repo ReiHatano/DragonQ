f252bb10404e032a9617bd39200291db3acc4430
ReiHatano <rei.hatano@gmail.com>
2015-12-03 22:46:08 +0900

    オルテガとシドーの対決作成開始

--- js/test.js
+++ js/test.js
@@ -1,12 +1,38 @@
-// document.write("abcd")
 
-var ortega = {name:"オルテガ", hp:70, strength:12}
-// var zoma = {name:"オルテガ", hp:70, strength:12}
+
+var ortega = {name:"オルテガ", hp:120, strength:12}
+var shidoh = {name:"シドー", hp:300, strength:18}
 
 var nameElement = document.getElementById("name");
-console.info(nameElement.tagName);
-console.info(nameElement.innerText);
-nameElement.innerHTML  = "ゾーマ"
+nameElement.innerHTML  = ortega.name
+
+var hpElement = document.getElementById("hp");
+hpElement.innerHTML = ortega.hp
+
+var strengthElement = document.getElementById("strength");
+strengthElement.innerHTML = ortega.strength
+
+function attack(a){
+  shidoh.hp -= a*2
+};
+
+attack(50);
+
+
+
+
+console.log(shidoh.hp);
+
+
+
+
+
+
+
+//console.info(nameElement.tagName);
+//console.info(nameElement.innerText);
+
+
 //console.info(name)
 
 // document.write("<p>"+ortega.name+"</p>")
