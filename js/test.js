932a880c8c44295af793ac3eec71d3bef9a34f3b
ReiHatano <rei.hatano@gmail.com>
2015-12-05 17:23:26 +0900

    戦闘システム無限ループ問題の見直し中

--- js/test.js
+++ js/test.js
@@ -1,7 +1,41 @@
 
+
+/*
+//オルテガの攻撃パターン
+var ortega_attack = function(){
+  var takeDamagePoint = (1+Math.floor(Math.random()*10)) + ortega.strength
+  shidoh.hp -= takeDamagePoint
+  document.write("<p>オルテガの攻撃！</p>"+shidoh.name+"に"+takeDamagePoint+"ポイントのダメージを与えた！");
+};
+
+//シドーの攻撃パターン
+var shidoh_attack = function(){
+  var takeDamagePoint = (1+Math.floor(Math.random()*10)) + shidoh.strength
+  ortega.hp -= takeDamagePoint
+  hpElement.innerHTML = ortega.hp
+  document.write("<p>シドーの攻撃！</p>"+ortega.name+"は"+takeDamagePoint+"ポイントのダメージを受けた！");
+};
+*/
+
 //キャラクター誕生
-var ortega = {name:"オルテガ", hp:120, strength:12, attack:function ortega_attack()}
-var shidoh = {name:"シドー", hp:300, strength:18, attack:function shidoh_attack()}
+var ortega =
+  {
+    name:"オルテガ", hp:120, strength:12,
+    attack : function(){
+      var takeDamagePoint = (1+Math.floor(Math.random()*10)) + ortega.strength
+      shidoh.hp -= takeDamagePoint
+      document.write("<p>オルテガの攻撃！</p>"+shidoh.name+"に"+takeDamagePoint+"ポイントのダメージを与えた！");
+    }
+  }
+
+var shidoh = {name:"シドー", hp:300, strength:18,
+  attack: function(){
+    var takeDamagePoint = (1+Math.floor(Math.random()*10)) + shidoh.strength
+    ortega.hp -= takeDamagePoint
+    hpElement.innerHTML = ortega.hp
+    document.write("<p>シドーの攻撃！</p>"+ortega.name+"は"+takeDamagePoint+"ポイントのダメージを受けた！");
+    }
+  }
 
 //ステータスの表示
 var nameElement = document.getElementById("name");
@@ -13,28 +47,18 @@ hpElement.innerHTML = ortega.hp
 var strengthElement = document.getElementById("strength");
 strengthElement.innerHTML = ortega.strength
 
-//オルテガの攻撃パターン
-function ortega_attack(){
-  var takeDamagePoint = (1+Math.floor(Math.random()*10)) + ortega.strength
-  shidoh.hp -= takeDamagePoint
-  document.write("<p>オルテガの攻撃！</p>"+shidoh.name+"に"+takeDamagePoint+"ポイントのダメージを与えた！");
-};
+
+
 /*
 function attack2(){
   var takeDamagePoint = ((1+Math.floor(Math.random()*10)) + ortega.strength)*2
   shidoh.hp -= takeDamagePoint
   document.write("<p>オルテガの攻撃！</p>会心の一撃！"+shidoh.name+"に"+takeDamagePoint+"ポイントのダメージを与えた！");
 };
-*/
 
-//シドーの攻撃パターン
-function shidoh_attack(){
-  var takeDamagePoint = (1+Math.floor(Math.random()*10)) + shidoh.strength
-  ortega.hp -= takeDamagePoint
-  hpElement.innerHTML = ortega.hp
-  document.write("<p>シドーの攻撃！</p>"+ortega.name+"は"+takeDamagePoint+"ポイントのダメージを受けた！");
-};
-/*
+
+
+
 function Sattack2(){
   var takeDamagePoint = (1+Math.floor(Math.random()*10)) + 30
   ortega.hp -= takeDamagePoint
@@ -44,19 +68,20 @@ function Sattack2(){
 */
 //戦闘システム
 var arr = [ortega, shidoh];
-function battle() {
+var battle =function(){
        var turn = 0;
        while (0 < arr[0].hp && 0 < arr[1].hp) {
 
            arr[turn].attack;
-
-           }
            turn = 1 - turn;
-       }
+           }
+
+       };
 
 
 //実行クラス
 battle();
+
 /*
 var arr = [ortega, shidoh];
 
@@ -70,4 +95,17 @@ Sattack2();
 
 console.log(shidoh.hp);
 console.log(ortega.hp);
+*/
+
+
+/*
+function charactor(name,hp,strength){
+  this.name = name;
+  this.hp = hp;
+  this.strength = strength;
+}
+
+var ortega = new charactor('オルテガ',120,12);
+
+console.log(ortega);
 */
