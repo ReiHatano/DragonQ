939755fdc759cfe7cfe5fd27b3ab50d1b0254c7d
ReiHatano <rei.hatano@gmail.com>
2015-12-06 10:29:03 +0900

    attack関数共通化済み(?)・・です
    なぜかhpが減らず無限ループ状態ですが、とりあえず甲斐さんの意図を反映したものになっているはずです！

--- js/test.js
+++ js/test.js
@@ -1,57 +1,10 @@
-
-
-/*
-//オルテガの攻撃パターン
-var ortega_attack = function(){
-  var takeDamagePoint = (1+Math.floor(Math.random()*10)) + ortega.strength
-  shidoh.hp -= takeDamagePoint
-  document.write("<p>オルテガの攻撃！</p>"+shidoh.name+"に"+takeDamagePoint+"ポイントのダメージを与えた！");
-};
-
-//シドーの攻撃パターン
-var shidoh_attack = function(){
-  var takeDamagePoint = (1+Math.floor(Math.random()*10)) + shidoh.strength
-  ortega.hp -= takeDamagePoint
-  hpElement.innerHTML = ortega.hp
-  document.write("<p>シドーの攻撃！</p>"+ortega.name+"は"+takeDamagePoint+"ポイントのダメージを受けた！");
-};
-*/
-
-
+//DragonQ_JsEdition
 
 //キャラクター誕生
-var ortega = {
-    name:"オルテガ", hp:120, strength:12,
-    /*attack :
-      function(){
-        var takeDamagePoint = (1+Math.floor(Math.random()*10)) + ortega.strength
-        shidoh.hp -= takeDamagePoint
-        document.write("<p>オルテガの攻撃！</p>"+shidoh.name+"に"+takeDamagePoint+"ポイントのダメージを与えた！");
-      },*/
-      attack2 :
-      function(){
-        var takeDamagePoint = ((1+Math.floor(Math.random()*10)) + ortega.strength)*3
-        shidoh.hp -= takeDamagePoint
-        document.write("<p>オルテガの攻撃！</p>会心の一撃！"+shidoh.name+"に"+takeDamagePoint+"ポイントのダメージを与えた！");
-      }
-}
+var ortega = {name:"オルテガ", hp:120, strength:12}
 
-var shidoh = {name:"シドー", hp:160, strength:18,
-  /*attack:
-    function(){
-    var takeDamagePoint = (1+Math.floor(Math.random()*10)) + shidoh.strength
-    ortega.hp -= takeDamagePoint
-    hpElement.innerHTML = ortega.hp
-    document.write("<p>シドーの攻撃！</p>"+ortega.name+"は"+takeDamagePoint+"ポイントのダメージを受けた！")
-  },*/
-  attack2:
-  function(){
-    var takeDamagePoint = (1+Math.floor(Math.random()*10)) + 30
-    ortega.hp -= takeDamagePoint
-    hpElement.innerHTML = ortega.hp
-    document.write("<p>シドーは燃え盛る火炎を吐いた！</p>"+ortega.name+"は"+takeDamagePoint+"ポイントのダメージを受けた！");
-  }
-}
+var shidoh = {name:"シドー", hp:160, strength:18}
+
 
 //ステータスの表示
 var nameElement = document.getElementById("name");
@@ -64,24 +17,12 @@ var strengthElement = document.getElementById("strength");
 strengthElement.innerHTML = ortega.strength
 
 
-
-/*
-function attack2(){
-  var takeDamagePoint = ((1+Math.floor(Math.random()*10)) + ortega.strength)*2
-  shidoh.hp -= takeDamagePoint
-  document.write("<p>オルテガの攻撃！</p>会心の一撃！"+shidoh.name+"に"+takeDamagePoint+"ポイントのダメージを与えた！");
-};
-
-
-
-
-function Sattack2(){
-  var takeDamagePoint = (1+Math.floor(Math.random()*10)) + 30
-  ortega.hp -= takeDamagePoint
-  hpElement.innerHTML = ortega.hp
-  document.write("<p>シドーは燃え盛る火炎を吐いた！</p>"+ortega.name+"は"+takeDamagePoint+"ポイントのダメージを受けた！");
-};
-*/
+//共通化済みアタック関数
+var attack = function(attackerStrength,defenderHp,attackerName,defenderName){
+  var takeDamagePoint = (1+Math.floor(Math.random()*10)) + attackerStrength
+  defenderHp -= takeDamagePoint
+  document.write("<p>"+attackerName+"の攻撃！</p>"+defenderName+"に"+takeDamagePoint+"ポイントのダメージ！")
+}
 
 
 //戦闘システム
@@ -92,26 +33,27 @@ var battle =function()
        var turn = 0;
        while (0 < arr[0].hp && 0 < arr[1].hp)
           {
-            var dice = (1+Math.floor(Math.random()*10))
-            console.log(dice)
-            if (0 < dice && dice < 6){
-              var attack = function(){
-                      var takeDamagePoint = (1+Math.floor(Math.random()*10)) + arr[turn].strength
-                      arr[1-turn].hp -= takeDamagePoint
-                      document.write("<p>"+arr[turn]+"の攻撃！</p>"+arr[1-turn].name+"に"+takeDamagePoint+"ポイントのダメージ！");
-                    };
-            }else{arr[turn].attack2();}
-
+            attack(arr[turn].strength,arr[1-turn].hp,arr[turn].name,arr[1-turn].name);
             turn = 1 - turn;
           };
-
-        if(arr[0].hp<0){arr[0].hp = 0}else{document.write("You Win")}
-
   };
 
 
 //実行クラス
+//attack(ortega.strength, shidoh.hp, ortega.name, shidoh.name);
+//attack(shidoh.strength, ortega.hp, shidoh.name, ortega.name);
 battle();
+
+
+
+
+
+
+
+
+//ここから下はソースの保管庫
+
+
 
 /*
 var arr = [ortega, shidoh];
@@ -139,4 +81,52 @@ function charactor(name,hp,strength){
 var ortega = new charactor('オルテガ',120,12);
 
 console.log(ortega);
+*/
+/*
+//オルテガの攻撃パターン
+var ortega_attack = function(){
+  var takeDamagePoint = (1+Math.floor(Math.random()*10)) + ortega.strength
+  shidoh.hp -= takeDamagePoint
+  document.write("<p>オルテガの攻撃！</p>"+shidoh.name+"に"+takeDamagePoint+"ポイントのダメージを与えた！");
+};
+
+attack2 :
+function(){
+  var takeDamagePoint = ((1+Math.floor(Math.random()*10)) + ortega.strength)*3
+  shidoh.hp -= takeDamagePoint
+  document.write("<p>オルテガの攻撃！</p>会心の一撃！"+shidoh.name+"に"+takeDamagePoint+"ポイントのダメージを与えた！");
+}
+
+//シドーの攻撃パターン
+var shidoh_attack = function(){
+  var takeDamagePoint = (1+Math.floor(Math.random()*10)) + shidoh.strength
+  ortega.hp -= takeDamagePoint
+  hpElement.innerHTML = ortega.hp
+  document.write("<p>シドーの攻撃！</p>"+ortega.name+"は"+takeDamagePoint+"ポイントのダメージを受けた！");
+};
+
+attack2:
+function(){
+  var takeDamagePoint = (1+Math.floor(Math.random()*10)) + 30
+  ortega.hp -= takeDamagePoint
+  hpElement.innerHTML = ortega.hp
+  document.write("<p>シドーは燃え盛る火炎を吐いた！</p>"+ortega.name+"は"+takeDamagePoint+"ポイントのダメージを受けた！");
+}
+
+*/
+
+/*
+//戦闘システム
+var arr = [ortega, shidoh];
+
+var battle =function()
+  {
+       var turn = 0;
+       while (0 < arr[0].hp && 0 < arr[1].hp)
+          {
+            arr[turn].attack()
+            turn = 1 - turn;
+          };
+
+  };
 */
